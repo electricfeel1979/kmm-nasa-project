432072a26818c371b7b5ea2cc5e4f7f2bf2dee79
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-11-30 17:02:29 +0800

    setup ts eslint prettier

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
-const http = require('http');
+import http from 'http';
 
-const app = require('./app');
+import app from './app';
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`)
-})
+  console.log(`Listening on port ${PORT}...`);
+});
