599690a9ee520ee456577703405f40c395cb5e1a
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-11-30 17:57:37 +0800

    110. CORS Middleware - 7min

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 
 import planetsRouter from './routes/planets/planets.router';
 
 const app = express();
 
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 app.use(express.json());
 app.use(planetsRouter);
 
